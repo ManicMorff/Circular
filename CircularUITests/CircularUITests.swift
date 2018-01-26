32fda4ff1ce1f2b3bd4b4384f4f8dc8dbf8458bd
McCallum, Steve <smc12@devml0195686.bskyb.com>
2018-01-26 11:34:09 +0000

    Initial Commit

---
+++ CircularUITests/CircularUITests.swift
@@ -0,0 +1,36 @@
+//
+//  CircularUITests.swift
+//  CircularUITests
+//
+//  Created by McCallum, Steve on 26/01/2018.
+//  Copyright © 2018 McCallum, Steve. All rights reserved.
+//
+
+import XCTest
+
+class CircularUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
